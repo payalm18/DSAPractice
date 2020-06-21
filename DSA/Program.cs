a1d94cdae5eea21fab9ea08a8f04a587fcca4a6e
Payal Mandal <payal18.m@gmail.com>
2020-06-21 12:57:32 +0530

    Demo question

---
+++ DSA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            int[] arrNum = new int[] { 6, 4,3, 1, 2 };
+            bool value= Demo.SumPair(arrNum, 9);
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+    public static class Demo
+    {
+        public static bool SumPair(int[] arraynum, int sum)
+        {
+            HashSet<int> myhash1 = new HashSet<int>();
+            for (int i = 0; i < arraynum.Length; i++)
+            {
+                if (myhash1.Contains(arraynum[i]))
+                {
+                    return true;
+                }
+                myhash1.Add(sum - arraynum[i]);
+            }
+            return false;
+        }
+    }
+}
