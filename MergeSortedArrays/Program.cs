e2a7fae0023ca881e72f4a2e12772f2af216b6e0
Payal Mandal <payal18.m@gmail.com>
2020-06-25 16:25:10 +0530

    Find index of SumPair

---
+++ MergeSortedArrays/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 3,4, 31 };
+            int[] b = new int[] { 4, 6, 30 };
+            int[] c = Demo.MergeSortedArrays(a, b);
+            foreach(int item in c)
+            {
+                Console.WriteLine(item + " " );
+            }
+            //Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+
+    static class Demo
+    {
+        public static int[] MergeSortedArrays(int[] arrSorted1, int[] arrSorted2)
+        {
+            int[] arrResultSorted = new int[arrSorted1.Length + arrSorted2.Length];
+            int i = 0, j = 0, k = 0;
+           
+            while (i < arrSorted1.Length  && j < arrSorted2.Length)
+            {
+                if (arrSorted1[i] < arrSorted2[j])
+                {
+                    arrResultSorted[k] = arrSorted1[i];
+                    i++;
+                    k++;
+                    
+                }
+                else
+                {
+                    arrResultSorted[k] = arrSorted2[j];
+                    j++;
+                    k++;
+                    
+                }
+
+                
+            }
+            while (i < arrSorted1.Length)
+            {
+                arrResultSorted[k++] = arrSorted1[i++];
+            }
+
+            while (j < arrSorted2.Length)
+            {
+                arrResultSorted[k++] = arrSorted2[j++];
+            }
+
+            return arrResultSorted;
+        }
+    }
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Today is a great day!";
+            string s = Demo.ReverseString(name);
+            Console.WriteLine(s);
+            string c = Demo.ReverseChar(name);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+
+    public static class Demo
+    {
+        //STRING WAY = O(n)
+        public static string ReverseString(string strRev)
+        {
+            //char[] strOriginalArray = strRev.ToCharArray();
+            string[] strOriginalArray = strRev.ToCharArray().Select(c => c.ToString()).ToArray();
+            string[] strRevArray = new string[strOriginalArray.Length];
+            string returnReverse = string.Empty;
+            int count = 0;
+            for (int i = strOriginalArray.Length-1; i >= 0; i--)
+            {
+                
+                strRevArray[count] = strOriginalArray[i];
+                count++;
+               
+
+
+            }
+            returnReverse =  string.Join("", strRevArray);
+            return returnReverse;
+        }
+
+        //CHAR WAY
+        public static string ReverseChar(string strRev)
+        {
+           // char[] strOriginalArray = strRev.ToCharArray();
+            char[] strRevArray = new char[strRev.Length];
+            string returnReverse = string.Empty;
+            int count = 0;
+            for (int i = strRev.Length - 1; i >= 0; i--)
+            {
+
+                strRevArray[count] = strRev[i];
+                count++;
+               // returnReverse = strRevArray.ToString();
+
+
+            }
+            //returnReverse = string.Join("", strRevArray);
+            return new string(strRevArray);
+        }
+    }
+}
